b920a054382497bb424873def38b6f2b1713d4f7
Nick Rogers and Swati Sahay <nick@>
2018-06-27 15:43:27 -0700

    Create Initial Project Folder.

---
+++ Allergies.Tests/ModelsTests/AllergiesTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Allergies;
+
+namespace Allergies.Tests
+{
+  [TestClass]
+  public class AllergiesTest
+  {
+    [TestMethod]
+    public void ExampleTest_True()
+    {
+      // eventually your testing code will go here
+    }
+  }
+}
---
+++ Allergies/Models/Allergies.cs
@@ -0,0 +1,11 @@
+namespace Allergies
+{
+public class Allergies
+  {
+  public bool ExampleTest(int year)
+    {
+      // logic will go here
+      return false;
+    }
+  }
+}
